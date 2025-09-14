1756590923644b7eb89c586e6f53ba93c7d017a2
haaum666 <moiraidrone@gmail.com>
2025-09-14 16:16:30 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-50">
+      <h1 className="text-4xl font-bold text-gray-800">Recruitlytics is ready!</h1>
+    </div>
+  );
+}
+
+export default App;
