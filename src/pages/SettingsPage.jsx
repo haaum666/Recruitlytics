c97b8b393019afaef0c888ffb83f0ddef08c8c8a
haaum666 <moiraidrone@gmail.com>
2025-09-14 17:20:56 +0300

    Create SettingsPage.jsx

---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SettingsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">Настройки</h1>
+      <p className="text-gray-600">
+        Здесь будет располагаться интерфейс для управления вопросами, их весами и шаблонами писем.
+      </p>
+    </div>
+  );
+}
+
+export default SettingsPage;
