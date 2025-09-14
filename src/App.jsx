b0cfdd66843eba4f6067c1c840d308b9a0ffd12e
haaum666 <moiraidrone@gmail.com>
2025-09-14 16:40:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Layout from './components/common/Layout';
+import AssessmentPage from './pages/AssessmentPage';
 
 function App() {
   return (
     <Layout>
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold text-gray-800">Recruitlytics is ready!</h1>
-      </div>
+      <AssessmentPage />
     </Layout>
   );
 }
