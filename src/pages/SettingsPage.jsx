0dcd34a63bef3df0c6b971b9b2fb125b4946001e
haaum666 <moiraidrone@gmail.com>
2025-09-14 22:37:32 +0300

    Update SettingsPage.jsx

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -70,7 +70,7 @@ function SettingsPage() {
   };
 
   return (
-    <div className="">
+    <div className="p-6 max-w-4xl mx-auto space-y-6">
       <h1 className="text-3xl font-bold mb-6">Настройки</h1>
 
       {/* Управление вопросами */}
