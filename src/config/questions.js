915044f58e04f7777da8d66dde545a1f3aa6c3f4
haaum666 <moiraidrone@gmail.com>
2025-09-14 16:53:28 +0300

    Create questions.js

---
+++ src/config/questions.js
@@ -0,0 +1,30 @@
+export const questions = [
+  {
+    id: 'hard_skills_1',
+    category: 'Hard Skills',
+    text: 'Опыт работы с React',
+    weight: 10,
+    type: 'boolean'
+  },
+  {
+    id: 'hard_skills_2',
+    category: 'Hard Skills',
+    text: 'Знание TypeScript',
+    weight: 10,
+    type: 'boolean'
+  },
+  {
+    id: 'soft_skills_1',
+    category: 'Soft Skills',
+    text: 'Умение работать в команде',
+    weight: 5,
+    type: 'boolean'
+  },
+  {
+    id: 'soft_skills_2',
+    category: 'Soft Skills',
+    text: 'Навыки решения проблем',
+    weight: 5,
+    type: 'boolean'
+  }
+];
