7ace317c3f3ec14950cb1313d3c81b15f6151e1f
haaum666 <moiraidrone@gmail.com>
2025-09-14 16:42:18 +0300

    Create AssessmentPage.jsx

---
+++ src/pages/AssessmentPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AssessmentPage() {
+  return (
+    <div className="flex flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold mb-4">Новая оценка</h1>
+      <p className="text-gray-600">Здесь будет располагаться чек-лист для оценки кандидата.</p>
+    </div>
+  );
+}
+
+export default AssessmentPage;
