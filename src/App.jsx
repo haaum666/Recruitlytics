f93f29f9b4f25bc7dad2afb179c39ced7c6f18d4
haaum666 <moiraidrone@gmail.com>
2025-09-14 17:21:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Layout from './components/common/Layout';
 import AssessmentPage from './pages/AssessmentPage';
 import HistoryPage from './pages/HistoryPage';
+import SettingsPage from './pages/SettingsPage';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('assessment');
@@ -12,6 +13,8 @@ function App() {
         return <AssessmentPage />;
       case 'history':
         return <HistoryPage />;
+      case 'settings':
+        return <SettingsPage />;
       default:
         return <AssessmentPage />;
     }
