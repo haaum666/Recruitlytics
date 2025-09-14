93b99adec3272a02f51ce85658392b21fdf5f54b
haaum666 <moiraidrone@gmail.com>
2025-09-14 17:13:49 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './components/common/Layout';
 import AssessmentPage from './pages/AssessmentPage';
+import HistoryPage from './pages/HistoryPage';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('assessment');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'assessment':
+        return <AssessmentPage />;
+      case 'history':
+        return <HistoryPage />;
+      default:
+        return <AssessmentPage />;
+    }
+  };
+
   return (
     <Layout>
-      <AssessmentPage />
+      {renderPage()}
     </Layout>
   );
 }
