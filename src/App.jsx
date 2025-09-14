7bcdd526bb5be0704099ec3b76df16ca25bd4637
haaum666 <moiraidrone@gmail.com>
2025-09-14 16:23:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './index.css';
+import Layout from './components/common/Layout';
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-50">
-      <h1 className="text-4xl font-bold text-gray-800">Recruitlytics is ready!</h1>
-    </div>
+    <Layout>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold text-gray-800">Recruitlytics is ready!</h1>
+      </div>
+    </Layout>
   );
 }
 
