7e149337b101faf72ee4f6a8be3976eab3516bb5
haaum666 <moiraidrone@gmail.com>
2025-09-16 02:05:07 +0300

    Update checkbox.jsx

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { cn } from "../../utils/cn";
 
-const Checkbox = React.forwardRef(({ className, checked, ...props }, ref) => (
+const Checkbox = React.forwardRef(({ className, checked, onCheckedChange, ...props }, ref) => (
   <button
     type="button"
     role="checkbox"
     aria-checked={checked}
     ref={ref}
+    onClick={() => onCheckedChange && onCheckedChange(!checked)}
     className={cn(
       "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className,
