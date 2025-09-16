872dd888586185970545223536e88d3adaaf65bb
haaum666 <moiraidrone@gmail.com>
2025-09-16 12:50:50 +0300

    Update Layout.jsx

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 
-function Layout({ children, onPageChange }) {
+function Layout({ children, onPageChange, currentPage }) {
   return (
     <div className="flex min-h-screen bg-slate-50">
-      <Sidebar onPageChange={onPageChange} />
-      <main className="flex-1 p-6">
+      <Sidebar onPageChange={onPageChange} currentPage={currentPage} />
+      <main className="flex-1 p-4">
         {children}
       </main>
     </div>
