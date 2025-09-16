6fb7966d36065e65fe40000f5c7eff67538ffcdb
haaum666 <moiraidrone@gmail.com>
2025-09-16 12:55:19 +0300

    Update Layout.jsx

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children, onPageChange, currentPage }) {
   return (
     <div className="flex min-h-screen bg-slate-50">
       <Sidebar onPageChange={onPageChange} currentPage={currentPage} />
-      <main className="flex-1 p-4">
+      <main className="flex-1 p-2">
         {children}
       </main>
     </div>
